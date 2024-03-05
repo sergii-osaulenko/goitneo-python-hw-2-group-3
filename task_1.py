6ba624a96c7e57bbd05b8ced6a10cfb8a1babe7d
sergii-osaulenko <sergii.osaulenko@gmail.com>
2024-03-05 21:54:03 +0200

    Add Original Code

--- task_1.py
+++ task_1.py
@@ -1,50 +1,51 @@
 def input_error(func):
-    def inner(*args, **kwargs):
+    def inner(*args):
         try:
-            return func(*args, **kwargs)
-        except ValueError:
-            return "Please provide a valid name and phone number."
+            return func(*args)
         except KeyError:
             return "Contact not found."
+        except ValueError:
+            return "Invalid input format."
         except IndexError:
-            return "Enter a valid command."
-
+            return "Insufficient arguments."
     return inner
-
-@input_error
-def add_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return f"Contact '{name}' added."
-
-@input_error
-def change_contact(args, contacts):
-    name, phone = args
-    if name in contacts:
-        contacts[name] = phone
-        return f"Contact '{name}' updated."
-    else:
-        return f"Contact '{name}' not found."
-
-@input_error
-def show_phone(args, contacts):
-    name = args[0]
-    if name in contacts:
-        return f"{name}'s phone number: {contacts[name]}"
-    else:
-        return f"Contact '{name}' not found."
-
-@input_error
-def show_all(contacts):
-    if not contacts:
-        return "No contacts found."
-    else:
-        return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
 
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, args
+
+@input_error
+def add_contact(args, contacts):
+    if len(args) != 2:
+        raise ValueError
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+    if len(args) != 2:
+        raise ValueError
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        raise KeyError
+
+@input_error
+def show_phone(args, contacts):
+    if len(args) != 1:
+        raise ValueError
+    name = args[0]
+    return contacts[name]
+
+def show_all(contacts):
+    if not contacts:
+        return "No contacts found."
+    else:
+        return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
 
 def main():
     contacts = {}
