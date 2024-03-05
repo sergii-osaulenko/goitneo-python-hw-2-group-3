e2a96c76bbcf8036982ddd6cdcc5df6cf886548a
sergii-osaulenko <sergii.osaulenko@gmail.com>
2024-03-05 20:15:19 +0200

    Edit "Good bye!"

--- task_1.py
+++ task_1.py
@@ -54,7 +54,7 @@ def main():
         command, args = parse_input(user_input)
 
         if command in ["close", "exit"]:
-            print("Goodbye!")
+            print("Good bye!")
             break
         elif command == "hello":
             print("How can I help you?")
